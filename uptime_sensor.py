baebb591e7d1842a592aa51c6cbba1f8440387c8
Hector Cordova <hectorcordova2024@gmail.com>
2024-01-09 20:31:08 -0500

    challenge02

--- uptime_sensor.py
+++ uptime_sensor.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
-import datetime
+import subprocess
+import time
+from datetime import datetime
 
+def ping_host(ip):
+    try:
+        subprocess.check_output(['ping', '-c', '1', ip], stderr=subprocess.STDOUT, text=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+    
+def main():
+    ip_address = '192.168.1.38'
+
+    while True:
+        timestamp = datetime.now().strftime(('%Y-%m-%d %H:%M:%S.%f')[:-3])
+        status = "Network Active" if ping_host(ip_address) else "Network Inactive"
+        print(f"{timestamp} {status} to {ip_address}")
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()
